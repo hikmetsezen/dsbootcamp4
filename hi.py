e55c6cf677edeb1da40f9cf36c7d3cd42bcad940
hikmetsezen <hikmetsezenn@gmail.com>
2021-02-10 13:39:28 +0100

    Hikmet Sezen ismi eklendi, bir bosluk silindi

--- hi.py
+++ hi.py
@@ -1,6 +1,7 @@
 print("hi my name is mvk")
 print('02: Anil Gevrek')
 print("02: Melih Akdag")
-print("03:  Pinar Dogan")
+print("03: Pinar Dogan")
 print("01: Ecem Bökenheide")
 print("03: Mürşide Yarkın")
+print("01: Hikmet Sezen")
